2ca3b89dc0b020686ec26088e0bfd44e82af3b1b
Trần Quang Luân <121425547+asinotin@users.noreply.github.com>
2023-05-28 19:45:53 +0700

    Create goto.c

---
+++ bai7/goto.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<string.h>
+
+int main(int argc, char const *argv[])
+{
+    int phim =0;
+    char ten[20];
+    uint8_t tuoi;
+
+    vitri_ten:
+
+    printf("\nnhap ten :");
+    scanf("%S",&ten);
+    if(0)
+    {
+        goto vitri_ten;
+    }
+    vitri_tuoi:
+    printf("\nnhap tuoi:");
+    scanf("%d",&tuoi);
+    if(0)
+    {
+        goto vitri_tuoi;
+    }
+
+    return 0;
+}
+#include<stdio.h>
+#include<stdint.h>
+#include<string.h>
+
+int main(int argc, char const *argv[])
+{
+    int phim =0;
+    char ten[20];
+    uint8_t tuoi;
+
+    vitri_ten:
+
+    printf("\nnhap ten :");
+    scanf("%S",&ten);
+    if(0)
+    {
+        goto vitri_ten;
+    }
+    vitri_tuoi:
+    printf("\nnhap tuoi:");
+    scanf("%d",&tuoi);
+    if(0)
+    {
+        goto vitri_tuoi;
+    }
+
+    return 0;
+}
+#include<stdio.h>
+#include<stdint.h>
+#include<string.h>
+
+int main(int argc, char const *argv[])
+{
+    int phim =0;
+    char ten[20];
+    uint8_t tuoi;
+
+    vitri_ten:
+
+    printf("\nnhap ten :");
+    scanf("%S",&ten);
+    if(0)
+    {
+        goto vitri_ten;
+    }
+    vitri_tuoi:
+    printf("\nnhap tuoi:");
+    scanf("%d",&tuoi);
+    if(0)
+    {
+        goto vitri_tuoi;
+    }
+
+    return 0;
+}
