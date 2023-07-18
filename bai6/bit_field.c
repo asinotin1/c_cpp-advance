6e459641e0f15c8067ffb59083fb53097af9127d
Trần Quang Luân <121425547+asinotin@users.noreply.github.com>
2023-07-18 10:52:17 +0700

    Create bit_field.c

---
+++ bai6/bit_field.c
@@ -0,0 +1,25 @@
+/*bit filed : giúp chúng ta tiết kiệm số byte trong 1 struct */
+// ví dụ trong struct test 1 : ta chỉ tốn 4 bit trong 1 struct vì nó lấy kiểu dữ liệu dầu tiên của biến trong struct
+#include <stdio.h>
+ 
+// A structure without forced alignment
+struct test1 {
+    unsigned int x : 5;
+    unsigned int y : 8;
+};
+ 
+// A structure with forced alignment
+struct test2 {
+    unsigned int x : 5;
+    unsigned int : 0;
+    unsigned int y : 8;
+};
+ 
+int main()
+{
+    printf("Size of test1 is %lu bytes\n",
+           sizeof(struct test1));
+    printf("Size of test2 is %lu bytes\n",
+           sizeof(struct test2));
+    return 0;
+}
