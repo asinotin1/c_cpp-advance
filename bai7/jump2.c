d6c2dda8a74c11dc79a19a24fc8e3402c4d8424b
Trần Quang Luân <121425547+asinotin@users.noreply.github.com>
2023-09-20 15:26:59 +0700

    Update jump2.c

--- bai7/jump2.c
+++ bai7/jump2.c
@@ -1,44 +1,45 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<setjmp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <setjmp.h>
 
 jmp_buf buf;
-int check_value=0;
+int check_value = 0;
 
-#define try if(check_value =setjmp(buf)==0)
+#define try if((check_value = setjmp(buf)) == 0)
 
-#define catch(num) else if(check_value==num)
+#define catch(num) else if (check_value == num)
 
-#define THROW(num) longjmp(buf,num)
+#define THROW(num) longjmp(buf, num)
 
-double thuong(int a,int b)
+double thuong(int a, int b)
 {
-    if(b==0)
+    if (b == 0)
     {
-        THROW(0);
+        THROW(1);
     }
-    if(b==5)
+    if (b == 5)
     {
         THROW(2);
     }
-    return (double)a/b;
+    return (double)a / b;
 }
-
 
 int main(int argc, char const *argv[])
 {
     double kq;
     try
     {
-        kq=thuong(12,5);
-        printf("kq=%f",kq);
-    }catch(1)
+        kq = thuong(12, 5);
+        printf("kq=%f", kq);
+    }
+    catch(1)
     {
         printf("Error mau bang 0");
-    }catch(2)
+    }
+    catch(2)
     {
         printf("ERROR mau bang 5");
     }
-    
+
     return 0;
 }
